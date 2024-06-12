17380e45ab62ef3bbec890ce12c7da6cd1b21509
Alejandro Verri <averri@gmail.com>
2024-06-12 18:01:36 -0300

    Added Rproj settings and improved ffread function
    - Updated .Rbuildignore to ignore Rproj files
    - Modified the ffread function in both the Rd and R files. The function now accepts optional 'index' and 'rows' parameters, which if not provided, will be computed as before. This change allows for more flexibility when calling the function.
    - Adjusted data reading in ffread to only read necessary rows, improving performance on large datasets.
    - Changed matching condition in ffread from '%in%' to '==' for exact match filtering.

--- R/ffread.R
+++ R/ffread.R
@@ -11,27 +11,37 @@
 #' @importFrom data.table data.table
 #' @importFrom data.table fread
 #' @examples
-ffread <- function(file, key, value, ...) {
+ffread <- function(file, key, value, index=NULL,rows=NULL,...) {
   tryCatch({
     # Read only the key column first
-    INDEX <- fread(file, select = key, ...)
-    
+    if(is.null(index)){
+      INDEX <- fread(file, select = key, ...)
+    } else {
+      INDEX <- index
+    }
     # Coerce value to strings for comparison
+    
     VALUE <- as.character(value)
-    
-    # Find all matching rows
-    ROWS <- which(INDEX[[key]] %in% VALUE)
-    
+    if(is.null(rows)){
+      ROWS <- which(INDEX[[key]] %in% VALUE)
+    } else {
+      ROWS <- rows
+    }
     if (length(ROWS) == 0) {
-      warning(sprintf("No rows found with key '%s' matching value '%s'", key, VALUE))
       return(NULL)
     }
+      
+    # Find all matching rows
+   
+    START <- max(1,first(ROWS)-10)
+    END <- min(nrow(INDEX),last(ROWS)+10)
+    NR <- END - START
     
     # Read the full data up to the last matching row
-    AUX <- fread(file, nrows = max(ROWS), ...)
+    AUX <- fread(file, skip = START - 1, nrows = END - START + 1, ...)
     
     # Filter the data table using %in% directly within the data table expression
-    return(AUX[get(key) %in% VALUE])
+    return(AUX[get(key) == VALUE])
   }, warning = function(w) {
     message("Warning: ", w$message)
     return(NULL)
